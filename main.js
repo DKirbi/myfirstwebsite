a47082b90484a2dbe24a949e226569cfdb4ebcbb
Davor <davor.kirbis@gmail.com>
2022-03-06 15:18:18 +0100

    First attempts on json loop

---
+++ main.js
@@ -0,0 +1,18 @@
+var pictures =  [
+    {
+        "name": "Škofja Loka Bridge",
+        "link": "https://live.staticflickr.com/65535/51565570257_0ab24e3457_o.jpg"
+    },
+    {
+        "name": "Škofja Loka Bridge",
+        "link": "https://live.staticflickr.com/65535/51565570257_0ab24e3457_o.jpg"
+    },
+    {
+        "name": "Škofja Loka Bridge",
+        "link": "https://live.staticflickr.com/65535/51565570257_0ab24e3457_o.jpg"
+    } 
+] 
+
+for(let picture of pictures) {
+    console.log(picture.name)
+}
